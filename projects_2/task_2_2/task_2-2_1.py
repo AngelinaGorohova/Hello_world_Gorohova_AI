d96808a90556f06b9e7cf7a169b9ecf01d2f5230
Горохова Ангелина <angelinkakeith@yandex.ru>
2026-02-25 23:15:12 +0300

    Create task_2-2_1.py

---
+++ projects_2/task_2_2/task_2-2_1.py
@@ -0,0 +1,5 @@
+name_of_course = "ИФОРМАТИКА"
+stage_number = "task_2-2"
+number_of_completed_tasks = 9
+
+print(f"Название курса: {name_of_course}.\nНомер текущего этапа: {stage_number}.\nКоличество выполненных задач: {number_of_completed_tasks}.")
